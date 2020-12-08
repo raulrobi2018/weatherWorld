296d73fd67ba432592e2de2d7c890a9282c494aa
rrodriguez <rrodriguez@cjpb.org.uy>
2020-12-08 19:44:12 -0300

    Class 56 - Get a request with Axios - Get a weather city

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { require } = require("yargs");
+const axios = require("axios");
 
 const argv = require('yargs').options({
     address: {
@@ -8,4 +8,23 @@ const argv = require('yargs').options({
     }
 }).argv;
 
+const encodedUrl = encodeURI(argv.address);
+const API_KEY = '67985f7525ef760712d9f5033933f10e';
+const unit = 'metric';
+
+const instance = axios.create({
+    baseURL: `https://api.openweathermap.org/data/2.5/weather?q=${encodedUrl}&appid=${API_KEY}&units=${unit}`
+  });
+
 console.log(argv.address);
+
+
+
+instance.get()
+.then((resp) => {
+    console.log(resp);
+    // console.log(resp.status);
+    // console.log(resp.data.main);
+    // console.log(resp.data.main);
+})
+.catch(err => console.log('An error has occurred', err));
